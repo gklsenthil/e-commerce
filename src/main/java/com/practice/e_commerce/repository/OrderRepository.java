fc02362dc2fa7f8fe596ceef29a00d29552a18cb
gklsenthil <gklsenthil15@gmail.com>
2025-01-10 16:16:00 +0530

    Deleted some codes

--- src/main/java/com/practice/e_commerce/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practice.e_commerce.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.practice.e_commerce.entity.Order;
-
-public interface OrderRepository extends MongoRepository<Order, String> {
-
-}
